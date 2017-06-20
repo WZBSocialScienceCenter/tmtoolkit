27b92c7c89dec367b6fa70a51784f9d87a8f242b
Markus Konrad <markus.konrad@wzb.eu>
2017-06-20 12:33:41 +0200

    setuptools skeleton

---
+++ setup.py
@@ -0,0 +1,53 @@
+"""
+tmtoolkit setuptools based setup module
+"""
+
+from setuptools import setup
+
+
+setup(
+    name='tmtoolkit',
+    version='0.1.0',
+
+    description='A set of tools for text mining / text analysis',
+    # TODO:
+#     long_description="""This repository contains a set of tools written in Python 3 with the aim to extract tabular
+# data from scanned and OCR-processed documents available as PDF files. Before these files can be processed they need
+# to be converted to XML files in pdf2xml format using poppler utils. Further information and examples can be found
+# in the github repository.""",
+#
+#     url='https://github.com/WZBSocialScienceCenter/pdftabextract',
+
+    author='Markus Konrad',
+    author_email='markus.konrad@wzb.eu',
+
+    license='Apache 2.0',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+
+    keywords='textmining textanalysis text mining analysis preprocessing',
+
+    packages=['tmtoolkit'],
+    include_package_data=True,
+    python_requires='>=2.7',
+    install_requires=['numpy', 'scipy', 'pandas', 'nltk'],
+    extras_require={
+        'improved_german_lemmatization':  ['pyphen', 'pattern'],
+    }
+)
