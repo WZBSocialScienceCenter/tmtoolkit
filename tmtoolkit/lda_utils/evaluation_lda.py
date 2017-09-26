16d07ac7680f2646707ed999a10e673eae8821e3
Markus Konrad <markus.konrad@wzb.eu>
2017-09-26 16:35:42 +0200

    lda eval n fold

--- tmtoolkit/lda_utils/evaluation_lda.py
+++ tmtoolkit/lda_utils/evaluation_lda.py
@@ -6,7 +6,7 @@ from scipy.sparse.coo import coo_matrix
 from lda import LDA
 
 
-from ._evaluation_common import merge_params, prepare_shared_data, start_multiproc_eval
+from ._evaluation_common import merge_params, prepare_shared_data, start_multiproc_eval, get_split_folds_array
 
 
 EVALUATE_LAST_LOGLIK = 0.05
@@ -17,15 +17,22 @@ shared_full_data = None
 shared_sparse_data = None
 shared_sparse_rows = None
 shared_sparse_cols = None
+shared_n_folds = None
+shared_split_folds = None
 
 
-def evaluate_topic_models(varying_parameters, constant_parameters, data, n_workers=None):
+def evaluate_topic_models(varying_parameters, constant_parameters, data, n_workers=None, n_folds=1):
     merged_params = merge_params(varying_parameters, constant_parameters)
     shared_full_data_base, shared_sparse_data_base,\
         shared_sparse_rows_base, shared_sparse_cols_base = prepare_shared_data(data)
 
+    if n_folds > 1:
+        split_folds = get_split_folds_array(n_folds, data.shape[0])
+    else:
+        split_folds = None
+
     initializer_args = (shared_full_data_base, shared_sparse_data_base, shared_sparse_rows_base,
-                        shared_sparse_cols_base, data.shape[0], data.shape[1])
+                        shared_sparse_cols_base, data.shape[0], data.shape[1], n_folds, split_folds)
     eval_results = start_multiproc_eval(n_workers, _init_shared_data, initializer_args,
                                         _fit_model_using_params, merged_params)
 
@@ -33,8 +40,9 @@ def evaluate_topic_models(varying_parameters, constant_parameters, data, n_worke
 
 
 def _init_shared_data(shared_full_data_base, shared_sparse_data_base, shared_sparse_rows_base,
-                      shared_sparse_cols_base, n_rows, n_cols):
-    global shared_full_data, shared_sparse_data, shared_sparse_rows, shared_sparse_cols
+                      shared_sparse_cols_base, n_rows, n_cols, n_folds, split_folds):
+    global shared_full_data, shared_sparse_data, shared_sparse_rows, shared_sparse_cols,\
+        shared_n_folds, shared_split_folds
 
     if shared_full_data_base is not None:
         shared_full_data = np.ctypeslib.as_array(shared_full_data_base.get_obj()).reshape(n_rows, n_cols)
@@ -52,6 +60,9 @@ def _init_shared_data(shared_full_data_base, shared_sparse_data_base, shared_spa
 
         shared_full_data = None
 
+    shared_n_folds = n_folds
+    shared_split_folds = split_folds
+
 
 def _fit_model_using_params(params):
     if shared_full_data is not None:
@@ -59,20 +70,42 @@ def _fit_model_using_params(params):
     else:
         full_dtm = coo_matrix((shared_sparse_data, (shared_sparse_rows, shared_sparse_cols)))
 
-    logger.info('fitting LDA model to data of shape %s with parameters: %s' % (full_dtm.shape, params))
+    if shared_n_folds is not None and shared_n_folds > 1:
+        logger.info('fitting LDA model with %d fold validation to data of shape %s with parameters: %s'
+                    % (shared_n_folds, full_dtm.shape, params))
 
-    lda_instance = LDA(**params)
-    lda_instance.fit(full_dtm)
+        perplexity_measurments = []
+        for cur_fold in range(shared_n_folds):
+            logger.info('> fold %d/%d' % (cur_fold+1, shared_n_folds))
+            dtm_train = full_dtm[shared_split_folds != cur_fold, :]
+            dtm_valid = full_dtm[shared_split_folds == cur_fold, :]
 
-    n_last_lls = max(int(round(EVALUATE_LAST_LOGLIK * len(lda_instance.loglikelihoods_))), 1)
+            lda_instance = LDA(**params)
+            lda_instance.fit(dtm_train)
 
-    logger.info('> done fitting model. will use mean of last %d log likelihood estimations for evaluation' % n_last_lls)
+            perpl_train = lda_instance.perplexity()               # evaluate "with itself"
+            perpl_valid = lda_instance.perplexity(dtm_valid)      # evaluate with held-out data
+            perpl_both = (perpl_train, perpl_valid)
 
-    if n_last_lls > 1:
-        report_ll = np.mean(lda_instance.loglikelihoods_[-n_last_lls:])
+            logger.info('> done fitting model. perplexity on training data: %f / on validation data: %f' % perpl_both)
+
+            perplexity_measurments.append(perpl_both)
+        return perplexity_measurments
     else:
-        report_ll = lda_instance.loglikelihoods_[-1]
+        logger.info('fitting LDA model to data of shape %s with parameters: %s' % (full_dtm.shape, params))
 
-    logger.info('> log likelihood: %f' % report_ll)
+        lda_instance = LDA(**params)
+        lda_instance.fit(full_dtm)
 
-    return report_ll
+        n_last_lls = max(int(round(EVALUATE_LAST_LOGLIK * len(lda_instance.loglikelihoods_))), 1)
+
+        logger.info('> done fitting model. will use mean of last %d log likelihood estimations for evaluation' % n_last_lls)
+
+        if n_last_lls > 1:
+            report_ll = np.mean(lda_instance.loglikelihoods_[-n_last_lls:])
+        else:
+            report_ll = lda_instance.loglikelihoods_[-1]
+
+        logger.info('> log likelihood: %f' % report_ll)
+
+        return report_ll
