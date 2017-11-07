abc263fe3f6fe3998ba4c5c5fb4dad8084d19d9a
Markus Konrad <markus.konrad@wzb.eu>
2017-11-07 10:57:01 +0100

    added lda_evaluation output updated PyPI description

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setup(
     version='0.1.1',
     description='Text Mining and Topic Modeling Toolkit',
     long_description="""tmtoolkit is a set of tools for text mining and topic modeling with Python. It contains
-    functions for text preprocessing like lemmatization, stemming or POS tagging especially for English and German
-    texts. Preprocessing is done in parallel by using all available processors on your machine. The topic modeling
-    features include topic model evaluation metrics, allowing to calculate models with different parameters in parallel
-    and comparing them (e.g. in order to find the best number of topics for a given set of documents). Topic models can
-    be generated in parallel for different copora and/or parameter sets using the LDA implementations either from
-    lda, scikit-learn or gensim.""",
+functions for text preprocessing like lemmatization, stemming or POS tagging especially for English and German
+texts. Preprocessing is done in parallel by using all available processors on your machine. The topic modeling
+features include topic model evaluation metrics, allowing to calculate models with different parameters in parallel
+and comparing them (e.g. in order to find the best number of topics for a given set of documents). Topic models can
+be generated in parallel for different copora and/or parameter sets using the LDA implementations either from
+lda, scikit-learn or gensim.""",
     url='https://github.com/WZBSocialScienceCenter/tmtoolkit',
 
     author='Markus Konrad',
