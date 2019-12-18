1d75dc47b2cda7bacabdcba7b12c09ce7c7eeaba
Markus Konrad <post@mkonrad.net>
2019-12-18 20:11:03 +0100

    this is 0.9.0-rc3

--- tmtoolkit/__init__.py
+++ tmtoolkit/__init__.py
@@ -8,6 +8,6 @@ from . import topicmod, bow, preprocess
 
 
 __title__ = 'tmtoolkit'
-__version__ = '0.9.0-rc2'
+__version__ = '0.9.0-rc3'
 __author__ = 'Markus Konrad'
 __license__ = 'Apache License 2.0'
