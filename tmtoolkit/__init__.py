eb151b96c035e1cbc204eddf742a2c883a4519f5
Markus Konrad <markus.konrad@wzb.eu>
2022-01-04 10:04:00 +0100

    WIP

--- tmtoolkit/__init__.py
+++ tmtoolkit/__init__.py
@@ -14,7 +14,6 @@ __license__ = 'Apache License 2.0'
 
 logger = logging.getLogger(__title__)
 logger.addHandler(logging.NullHandler())
-logger.setLevel(logging.WARNING)
 
 from . import bow, topicmod, tokenseq, types, utils
 
