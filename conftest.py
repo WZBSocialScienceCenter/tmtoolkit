c98881e7de83c12c900f68142f90d62d0afe4506
Markus Konrad <markus.konrad@wzb.eu>
2022-02-10 12:20:41 +0100

    increase hypothesis timeout deadline for CI runs

--- conftest.py
+++ conftest.py
@@ -7,4 +7,5 @@ Configuration for tests with pytest
 from hypothesis import settings, HealthCheck
 
 # profile for CI runs on GitHub machines, which may be slow from time to time so we disable the "too slow" HealthCheck
-settings.register_profile('ci', suppress_health_check=(HealthCheck.too_slow, ))
+# and set the timeout deadline very high (60 sec.)
+settings.register_profile('ci', suppress_health_check=(HealthCheck.too_slow, ), deadline=60000)
